eb35bf524a3e5fd18aa144c63c6af5140a18df82
dliu888 <78522097+dliu888@users.noreply.github.com>
2021-02-18 23:48:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,10 @@
 
 //The main function should read two ints from the command line using the atoi function from the cstdlib library.
 
-int main ()
+int main (int argc, char** argv[])
 {
-    int x, y;
-    x = atoi(cin >> x);  //takes user input from command line and converts from string to int type
-    y = atoi(cin >> y);  //stores x and y values into array of argv at positions 1 and 2
+    int x = atoi(argv[1]);  //takes user input from command line and converts from string to int type
+    int y = atoi(argv[2]);  //stores x and y values into array of argv at positions 1 and 2
     bool same = false;
     bool swapped = false; //both bool initialized to false by default; we use this for comparing x and y values
 
@@ -19,7 +18,7 @@ int main ()
 
     makeFirstBigger(x, y, swapped);
 
-    if(same) //if same= true, executes first branch of code, other false and executes 2nd branch of code
+    if(same) //if same = true, executes first branch of code, other false and executes 2nd branch of code
     {
         std::cout << "x and y are the same" << std::endl;
     } 
@@ -36,5 +35,13 @@ int main ()
     {
         std::cout << "x and y were not swapped because they were already in the right order." << std::endl;
     }
-    return 0;
+    
+    //if values of x and y are not the same..
+     if(!same)
+   {
+       int previousNumber = makeBothSame(x, y);
+       std::cout << "Now x and y are both equal to " << x << "Previously, y's value was " << previousNumber << std::endl;
+   }
+   return 0;
+
 }
